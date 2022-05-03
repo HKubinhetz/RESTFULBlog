96198568ee223acc1d19b7766860da7c326b32ea
Henrique Kubinhetz <rickubinhetz@gmail.com>
2022-05-03 11:07:37 -0300

    Add New Post functionality

--- blog_timing.py
+++ blog_timing.py
@@ -1,5 +1,14 @@
 import datetime
 
-print(datetime.date.today())
 
-# TODO implement date into the format: August 31, 2019
+def get_post_timing():
+    current_day = datetime.date.today().strftime("%d")
+    current_month = datetime.date.today().strftime("%B")
+    current_year = datetime.date.today().strftime("%Y")
+
+    return f"{current_month} {current_day}, {current_year}"
+
+
+if __name__ == "__main__":
+    current_time = get_post_timing()
+    print(current_time)
